f64a2b10aff4b489a011ebe480a194c5d28b3eed
SaBobic <fabrizioettori@gmail.com>
2022-06-27 17:19:36 +0200

    connected prev/next arrows with thumbnail list scroll

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -9,7 +9,9 @@
 
 // 1. Dichiaro le variabili globali: targhet HTML in cui inserire la lista, stringa rappresentante la lista da inserire nel target
 const carouselGallery = document.getElementById('main-gallery');
+const carouselThumbnail = document.getElementById('main-thumbnail-carousel');
 let galleryList = '<i class="fa-solid fa-circle-chevron-left" id="prev-arrow"></i><ul>';
+let thumbnailList = '<ul>';
 
 // 2. Creo un array contenete le immagini da inserire nei vari li
 const images = ["img/01.jpg", "img/02.jpg", "img/03.jpg", "img/04.jpg", "img/05.jpg"];
@@ -17,12 +19,15 @@ const images = ["img/01.jpg", "img/02.jpg", "img/03.jpg", "img/04.jpg", "img/05.
 // 3. Creo ciclo for i=0, i<5 per concatenare i li all'interno degli ul (uso i per centralizzare nome immagini)
 for (let i = 0; i < images.length; i++){
     galleryList += `<li><img src="${images[i]}" alt="Gallery Image ${i + 1}"></li>`;
+    thumbnailList += `<li><img src="${images[i]}" alt="Gallery Image ${i + 1}"></li>`;
 }
 
 galleryList += '</ul><i class="fa-solid fa-circle-chevron-right" id="next-arrow"></i>';
+thumbnailList += '</ul>';
 
 // 4. Stampo in pagina la lista HTML creata
 carouselGallery.innerHTML = galleryList;
+carouselThumbnail.innerHTML = thumbnailList;
 
 //* MILESTONE 3: Al click dell'utente sulle frecce, l'immagine attiva cambia e diventa visibile nello slider, prendendo il posto della precedente
 
@@ -40,10 +45,12 @@ carouselGallery.innerHTML = galleryList;
 const prevArrow = document.getElementById('prev-arrow');
 const nextArrow = document.getElementById('next-arrow');
 const galleryListImages = document.querySelectorAll('#main-gallery img');
+const thumbnailListImages = document.querySelectorAll('#main-thumbnail-carousel img');
 let activeClassIndex = 0;
 
 // 2. Rendo visibile la prima immagine dell'array inserendo la classe CSS "active"
 galleryListImages[activeClassIndex].classList.add('active');
+thumbnailListImages[activeClassIndex].classList.add('active');
 
 //* Next arrow
 
@@ -52,23 +59,31 @@ nextArrow.addEventListener('click', function(){
     
     // 4. All'interno dell'addEventListener quando clicco il bottone faccio rimuovere la classe active all'immagine che già la possiede
     galleryListImages[activeClassIndex].classList.remove('active');
+    thumbnailListImages[activeClassIndex].classList.remove('active');
     
     // 5. All'interno dell'addEventListener quando clicco modifico (+1) l'activeClassIndex
     activeClassIndex++;
 
     // ! BONUS 1: Aggiungere il ciclo infinito del carosello
     if (activeClassIndex === images.length) activeClassIndex = 0;
+    if (activeClassIndex === images.length) activeClassIndex = 0;
     
     // 6. All'interno dell'addEventListener quando clicco associo nuovamente la classe active secondo l'activeClassIndex
     galleryListImages[activeClassIndex].classList.add('active');
+    thumbnailListImages[activeClassIndex].classList.add('active');
 });
 
 //* Prev arrow
 
 prevArrow.addEventListener('click', function(){
     galleryListImages[activeClassIndex].classList.remove('active');
+    thumbnailListImages[activeClassIndex].classList.remove('active');
     activeClassIndex--;
     // ! BONUS 1: Aggiungere il ciclo infinito del carosello
     if (activeClassIndex < 0) activeClassIndex = images.length - 1;
+    if (activeClassIndex < 0) activeClassIndex = images.length - 1;
     galleryListImages[activeClassIndex].classList.add('active');
+    thumbnailListImages[activeClassIndex].classList.add('active');
 });
+
+// ! BONUS 2: Animare il carosello delle thumbnail, connettendolo allo stesso array della main-gallery
